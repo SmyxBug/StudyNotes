6a9b761ae50eebd7cc8b240dff124b6670bade37
SmyxCoding <itchangelives@126.com>
2020-09-13 22:37:06 +0800

    学习记录提交

--- JavaScript/es6/promise_example/app.js
+++ JavaScript/es6/promise_example/app.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+// 原始写法
+fs.readFile('./files/test.txt', (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+// 使用Promise写法
 const p = new Promise(function(resolve, reject) {
   fs.readFile('./files/test.txt', (err, data) => {
     if(err) reject(err);
